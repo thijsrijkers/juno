3cd6879545f280ab144c8f775a54876936f6adb2
ThijsRijkersCoolblue <thijs.rijkers@coolblue.nl>
2026-03-10 15:14:24 +0100

    Updated lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,3 @@
--- plugins/lsp.lua
 return {
   -- Mason: automatically installs LSP servers
   {
@@ -23,8 +22,8 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
-          "gopls",        -- Go
-          "ts_ls",        -- TypeScript & JavaScript
+          "gopls",   -- Go
+          "ts_ls",   -- TypeScript & JavaScript
         },
         automatic_installation = true,
       })
@@ -39,29 +38,31 @@ return {
       "williamboman/mason-lspconfig.nvim",
     },
     config = function()
-      local lspconfig = require("lspconfig")
-
       -- Shared on_attach for keymaps
       local on_attach = function(_, bufnr)
         local map = function(keys, func, desc)
           vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
         end
 
-        map("gd", vim.lsp.buf.definition,      "Go to Definition")
-        map("gD", vim.lsp.buf.declaration,     "Go to Declaration")
-        map("gr", vim.lsp.buf.references,      "Go to References")
-        map("gi", vim.lsp.buf.implementation,  "Go to Implementation")
-        map("K",  vim.lsp.buf.hover,           "Hover Documentation")
-        map("<leader>rn", vim.lsp.buf.rename,  "Rename")
+        map("gd", vim.lsp.buf.definition,          "Go to Definition")
+        map("gD", vim.lsp.buf.declaration,         "Go to Declaration")
+        map("gr", vim.lsp.buf.references,          "Go to References")
+        map("gi", vim.lsp.buf.implementation,      "Go to Implementation")
+        map("K",  vim.lsp.buf.hover,               "Hover Documentation")
+        map("<leader>rn", vim.lsp.buf.rename,      "Rename")
         map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-        map("<leader>lf", function() vim.lsp.buf.format({ async = true }) end, "Format file")
+        map("<leader>lf", function()
+          vim.lsp.buf.format({ async = true })
+        end, "Format file")
       end
 
       -- Go
-      lspconfig.gopls.setup({ on_attach = on_attach })
+      vim.lsp.config("gopls", { on_attach = on_attach })
+      vim.lsp.enable("gopls")
 
       -- TypeScript & JavaScript
-      lspconfig.ts_ls.setup({ on_attach = on_attach })
+      vim.lsp.config("ts_ls", { on_attach = on_attach })
+      vim.lsp.enable("ts_ls")
     end,
   },
 }
